9b96a2aefade1d765b31ce0b3da9cb5cec49e232
Gabor Horvath <xazax.hun@gmail.com>
2017-03-31 18:08:53 +0200

    Addressed some commit reviews.

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -1782,7 +1782,6 @@ Decl *ASTNodeImporter::VisitEnumConstantDecl(EnumConstantDecl *D) {
   DeclarationName Name;
   SourceLocation Loc;
   NamedDecl *ToD;
-
   if (ImportDeclParts(D, DC, LexicalDC, Name, ToD, Loc))
     return nullptr;
   if (ToD)
