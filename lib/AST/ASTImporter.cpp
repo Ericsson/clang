f22843c1a24fd8ba8aa12d6d8893bea2be6ef716
Peter Szecsi <szepet95@gmail.com>
2017-08-02 19:08:33 +0200

    FriendDecl importing improvements D32947

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -2381,8 +2381,12 @@ Decl *ASTNodeImporter::VisitFriendDecl(FriendDecl *D) {
 
   // Not found. Create it.
   FriendDecl::FriendUnion ToFU;
-  if (NamedDecl *FriendD = D->getFriendDecl())
-    ToFU = cast_or_null<NamedDecl>(Importer.Import(FriendD));
+  if (NamedDecl *FriendD = D->getFriendDecl()) {
+    auto ToFriendD = cast_or_null<NamedDecl>(Importer.Import(FriendD));
+    if (ToFriendD && FriendD->getFriendObjectKind())
+      ToFriendD->setObjectOfFriendDecl();
+     ToFU = ToFriendD;
+  }
   else
     ToFU = Importer.Import(D->getFriendType());
   if (!ToFU)
@@ -2404,7 +2408,6 @@ Decl *ASTNodeImporter::VisitFriendDecl(FriendDecl *D) {
                                        ToTPLists);
 
   Importer.Imported(D, FrD);
-  RD->pushFriendDecl(FrD);
 
   FrD->setAccess(D->getAccess());
   FrD->setLexicalDeclContext(LexicalDC);
