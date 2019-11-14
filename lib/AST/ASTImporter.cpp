6127b35428ff2b182a74a28b894666d9ac54a059
Gabor Marton <gabor.marton@ericsson.com>
2019-11-14 15:08:14 +0100

    D67543 [ASTImporter] Added visibility check for ClassTemplateDecl.

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -5113,6 +5113,8 @@ ExpectedDecl ASTNodeImporter::VisitClassTemplateDecl(ClassTemplateDecl *D) {
       Decl *Found = FoundDecl;
       auto *FoundTemplate = dyn_cast<ClassTemplateDecl>(Found);
       if (FoundTemplate) {
+        if (!hasSameVisibilityContext(FoundTemplate, D))
+          continue;
 
         if (IsStructuralMatch(D, FoundTemplate)) {
           ClassTemplateDecl *TemplateWithDef =
